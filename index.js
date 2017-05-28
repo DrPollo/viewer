9514f698aa7315c1952face6ca3be1533d9574d5
Alessio <antonini@di.unito.it>
2017-05-28 19:13:40 +0200

    visualizzazione gaussiana e colori

--- index.js
+++ index.js
@@ -4,18 +4,19 @@
 require('leaflet');
 require('leaflet.vectorgrid');
 require('./libs/leaflet-geojson-gridlayer');
+require('gaussian');
 
 // defaults
-var zoom = 13;
-var lat = 45.070312;
-var lon = 7.686856;
+var initZoom = 13;
+var initLat = 45.070312;
+var initLon = 7.686856;
 var baselayer = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
 var contrastlayer = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png';
 
 
 var orange = "#ff7800";
 
-
+var gaussian = require('gaussian');
 //
 // L.mapbox.accessToken = 'pk.eyJ1IjoiZHJwMGxsMCIsImEiOiI4bUpPVm9JIn0.NCRmAUzSfQ_fT3A86d9RvQ';
 // var map = L.mapbox.map('map');
@@ -58,13 +59,18 @@ var layers = {
 };
 
 
-var map = L.map('map').setView([lat, lon], zoom );
+var map = L.map('map').setView([initLat, initLon], initZoom );
 layers[contrast ? 'contrast': 'base'].addTo(map);
 
 
 
 
-
+var colors = {
+    'FL_GROUPS':'#3F7F91',
+    'FL_EVENTS':'#88BA5C',
+    'FL_NEWS':'#823256',
+    'FL_ARTICLES':'#FFB310',
+    'FL_PLACES':'#FE4336'};
 
 
 
@@ -110,33 +116,64 @@ var vectormapUrl = "https://tiles.firstlife.org/tile/{z}/{x}/{y}";
 // definition of the vectorGrid layer
 // var vGrid = L.vectorGrid.protobuf(vectormapUrl, vectormapConfig).addTo(map);
 
-var scale = function(x){
-    // https://www.desmos.com/calculator/3fisjexbvp
-    // return Math.log(num*10);
-    var a = 0.05,
-    b = 1.33,
-    c = 0;
-    return Math.floor(a*(Math.pow(b,x))+c);
+
+// exponential
+// var scale = function(x,level){
+//
+//     if(x > level)
+//         return 0;
+//     // https://www.desmos.com/calculator/3fisjexbvp
+//     // return Math.log(num*10);
+//     var a = 0.05,
+//     b = 1.33,
+//     c = 0;
+//     return Math.floor(a*(Math.pow(b,x))+c);
+// }
+
+
+/*
+ * Gaussian
+ * a = max radius
+ * b = zoom level
+ * c = spread (between 2 - 3)
+ */
+var maxWeight = 2;
+var maxRadius = 10,
+    c = 1.4;
+var scale = function(x,level){
+    var k = Math.pow((x - level),2) * -1;
+    var q = 2 * Math.pow(c,2);
+    var z = k/q;
+    return Math.floor(maxRadius * Math.exp(z));
 }
 /*
  * Markers
  */
 var geojsonMarkerOptions = {
-    radius: scale(zoom),
-    fillColor: orange,
-    color: orange,
-    weight: 2,
     opacity: 1,
     fillOpacity: 0.8
 };
 
-var markerUrl = 'https://api.firstlife.org/v5/fl/Things/tilesearch?domainId=1,9,10,11,12,13,14,15&limit=99999&tiles={x}:{y}:{z}';
+var markerUrl = 'https://api.fldev.di.unito.it/v5/fl/Things/tilesearch?domainId=1,9,10,11,12,13,14,15&limit=99999&tiles={x}:{y}:{z}';
 var layers = {
     'layers':{
         'things':{
-            pointToLayer:function (properties,latlng) {
-                console.log(properties,latlng);
-                return L.circleMarker(latlng,geojsonMarkerOptions);
+            pointToLayer:function (feature,latlng) {
+                var type = feature.properties.entity_type;
+                var color = colors[type];
+                var radius = scale(initZoom,feature.properties.zoom_level);
+                var weight = Math.min(radius,maxWeight);
+                var style = Object.assign(
+                    {},
+                    geojsonMarkerOptions,
+                    {
+                        radius: radius,
+                        weight: weight,
+                        color: color,
+                        fillColor: color
+                    }
+                );
+                return L.circleMarker(latlng,style);
             }
         }
     }
@@ -147,15 +184,16 @@ map.addLayer(mGrid);
 
 map.on('zoomend',function (e) {
     var layer = mGrid.getLayers()[0];
-    console.log(layer);
+    // console.log(layer);
     var features = layer._layers;
     var zoom = map.getZoom();
     // console.log('nuovo raggio: ',scale(zoom));
     for(var i in features){
-        console.log(features[i].feature.properties);
-        var z = features[i].feature.properties.zoom_level;
-        features[i].setRadius(scale(zoom));
-        // features[i].setRadius(Math.floor(zoom/2));
+        // console.log(features[i].feature.properties.zoom_level);
+        var level = features[i].feature.properties.zoom_level;
+        var radius = scale(zoom,level);
+        features[i].setRadius(radius);
+        features[i].setStyle({weight:Math.min(radius,maxWeight)});
     }
 });
 
