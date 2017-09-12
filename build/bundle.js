abe955cfc724bb5b95c82670d1b8ddbd11526d44
Alessio <antonini@di.unito.it>
2017-09-12 16:10:04 +0200

    definition of areaViewer

--- build/bundle.js
+++ build/bundle.js
@@ -1,192 +1,195 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/**
- * Created by drpollo on 21/05/2017.
- */
-// librerie
-require('leaflet');
+const areaViewer = function () {
+    /**
+     * Created by drpollo on 21/05/2017.
+     */
+    // librerie
+    require('leaflet');
 
-// librerie ad hoc
-require('./libs/Leaflet.VectorGrid');
-require('./libs/leaflet-geojson-gridlayer');
+    // librerie ad hoc
+    require('./libs/Leaflet.VectorGrid');
+    require('./libs/leaflet-geojson-gridlayer');
 
-const within = require('@turf/within');
-const turf = require('@turf/helpers');
-/*
- * moduli
- */
+    const within = require('@turf/within');
+    const turf = require('@turf/helpers');
+    /*
+     * moduli
+     */
 
-// gestore di stato
-const Status = require('./js/status');
-const status = Status();
-// mappa generale
-const Map = require('./js/map');
-const map = Map();
+    // gestore di stato
+    const Status = require('./js/status');
+    const status = Status();
+    // mappa generale
+    const Map = require('./js/map');
+    const map = Map();
 
-const markerGrid = require('./js/datasource.js');
-const mGrid = markerGrid(map);
+    const markerGrid = require('./js/datasource.js');
+    const mGrid = markerGrid(map);
 
-const vectorGrid = require('./js/interactive.js');
-const vGrid = vectorGrid();
+    const vectorGrid = require('./js/interactive.js');
+    const vGrid = vectorGrid();
 
-const focusLayer = require('./js/focus');
-const fLayer = focusLayer();
+    const focusLayer = require('./js/focus');
+    const fLayer = focusLayer();
 
-// utilities
-const Utils = require('./js/utils');
-const utils = Utils();
+    // utilities
+    const Utils = require('./js/utils');
+    const utils = Utils();
 
-/*
- * costanti e defaults
- */
-// colori
-const colors = {
-    'FL_GROUPS': '#3F7F91',
-    'FL_EVENTS': '#88BA5C',
-    'FL_NEWS': '#823256',
-    'FL_ARTICLES': '#FFB310',
-    'FL_PLACES': '#FE4336'
-};
-const orange = "#ff7800",
-      blue = "#82b1ff",
-      green = "#33cd5f",
-      gray = "#dcdcdc";
+    /*
+     * costanti e defaults
+     */
+    // colori
+    const colors = {
+        'FL_GROUPS': '#3F7F91',
+        'FL_EVENTS': '#88BA5C',
+        'FL_NEWS': '#823256',
+        'FL_ARTICLES': '#FFB310',
+        'FL_PLACES': '#FE4336'
+    };
+    const orange = "#ff7800",
+          blue = "#82b1ff",
+          green = "#33cd5f",
+          gray = "#dcdcdc";
 
-// const baselayer = '';
-const baselayer = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
-const contrastlayer = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png';
-// default contrast
-let contrast = false;
+    // const baselayer = '';
+    const baselayer = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
+    const contrastlayer = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png';
+    // default contrast
+    let contrast = false;
 
-// language
-const defaultLang = 0;
-const languages = ['en', 'it'];
-const userLang = navigator.language || navigator.userLanguage;
-let lang = languages[defaultLang];
-for (let i = 0; i < languages.length; i++) {
-    let l = languages[i];
-    if (userLang.search(l) > -1) {
-        lang = l;
+    // language
+    const defaultLang = 0;
+    const languages = ['en', 'it'];
+    const userLang = navigator.language || navigator.userLanguage;
+    let lang = languages[defaultLang];
+    for (let i = 0; i < languages.length; i++) {
+        let l = languages[i];
+        if (userLang.search(l) > -1) {
+            lang = l;
+        }
     }
-}
 
-// default date
-let date = new Date();
-// get search params
-// check for IE
-const ua = window.navigator.userAgent;
-const msie = ua.indexOf("MSIE ");
+    // default date
+    let date = new Date();
+    // get search params
+    // check for IE
+    const ua = window.navigator.userAgent;
+    const msie = ua.indexOf("MSIE ");
 
-// If Internet Explorer, return version number
-if (msie > 0) {
-    params = escape(location.search);
-} else {
-    params = new URL(location).searchParams;
-}
-
-if (params) {
-    // override location from get params
-    // c = lat:lng:zoom > centre
-    if (params.get("c")) {
-        let c = params.get("c");
+    // If Internet Explorer, return version number
+    if (msie > 0) {
+        params = escape(location.search);
+    } else {
+        params = new URL(location).searchParams;
     }
-    // contrast
-    contrast = params.get('contrast') === 'true';
-    // lang > default agent or "en"
-    lang = params.get('lang') ? params.get('lang') : lang;
-    // date > current date
-    date = params.get("date") ? params.get("date") : date;
-} else {
-    console.error('cannot retrieve search params from URL location');
-}
 
-// map setup
-const layers = {
-    base: L.tileLayer(baselayer, {
-        maxZoom: 20,
-        attribution: '<a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors | <a href="http://mapbox.com" target="_blank">Mapbox</a>'
-    }),
-    contrast: L.tileLayer(contrastlayer, {
-        maxZoom: 20,
-        attribution: '<a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors | <a href="http://mapbox.com" target="_blank">Mapbox</a>'
-    })
-};
-// cartography
-layers[contrast ? 'contrast' : 'base'].addTo(map);
+    if (params) {
+        // override location from get params
+        // c = lat:lng:zoom > centre
+        if (params.get("c")) {
+            let c = params.get("c");
+        }
+        // contrast
+        contrast = params.get('contrast') === 'true';
+        // lang > default agent or "en"
+        lang = params.get('lang') ? params.get('lang') : lang;
+        // date > current date
+        date = params.get("date") ? params.get("date") : date;
+    } else {
+        console.error('cannot retrieve search params from URL location');
+    }
 
-/*
- * Inizializzazioni
- */
-// stato
-status.move(map.getBounds());
-// inizializzazione vectorGrid layer
-vGrid.addTo(map);
-// inizializzazione markerGrid layer
-mGrid.addTo(map);
-// inizializzazione focusLayer
-fLayer.addTo(map);
+    // map setup
+    const layers = {
+        base: L.tileLayer(baselayer, {
+            maxZoom: 20,
+            attribution: '<a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors | <a href="http://mapbox.com" target="_blank">Mapbox</a>'
+        }),
+        contrast: L.tileLayer(contrastlayer, {
+            maxZoom: 20,
+            attribution: '<a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors | <a href="http://mapbox.com" target="_blank">Mapbox</a>'
+        })
+    };
+    // cartography
+    layers[contrast ? 'contrast' : 'base'].addTo(map);
 
-/*
- * gestione del focus
- */
-// fit to bounds
-// valuta se fare fix dello zoom > options.maxZoom = map.getCenter();
-status.observe.filter(state => 'bounds' in state).map(state => state.bounds).subscribe(bounds => {
-    // map.removeLayer(mGrid);
-    map.fitBounds(bounds);
-    // map.addLayer(mGrid);
-});
-// draw focus border
-status.observe.filter(state => 'id' in state).map(state => state.id).subscribe(id => vGrid.highlight(id));
-// set default style
-status.observe.filter(state => 'id' in state).map(state => state.id).subscribe(id => mGrid.setStyle(id));
-// add focus layer
-status.observe.filter(state => 'features' in state).map(state => state.features).subscribe(features => fLayer.setLayer(features));
-// reset del focus
-status.observe.filter(state => 'reset' in state).subscribe(() => {
-    mGrid.resetStyle();
-    vGrid.resetStyle();
-    fLayer.clearLayers();
-});
+    /*
+     * Inizializzazioni
+     */
+    // stato
+    status.move(map.getBounds());
+    // inizializzazione vectorGrid layer
+    vGrid.addTo(map);
+    // inizializzazione markerGrid layer
+    mGrid.addTo(map);
+    // inizializzazione focusLayer
+    fLayer.addTo(map);
 
-/*
- * Gestione eventi mappa
- */
-vGrid.on('click', e => {
-    if (e.originalEvent.defaultPrevented) return;
+    /*
+     * gestione del focus
+     */
+    // fit to bounds
+    // valuta se fare fix dello zoom > options.maxZoom = map.getCenter();
+    status.observe.filter(state => 'bounds' in state).map(state => state.bounds).subscribe(bounds => {
+        // map.removeLayer(mGrid);
+        map.fitBounds(bounds);
+        // map.addLayer(mGrid);
+    });
+    // draw focus border
+    status.observe.filter(state => 'id' in state).map(state => state.id).subscribe(id => vGrid.highlight(id));
+    // set default style
+    status.observe.filter(state => 'id' in state).map(state => state.id).subscribe(id => mGrid.setStyle(id));
+    // add focus layer
+    status.observe.filter(state => 'features' in state).map(state => state.features).subscribe(features => fLayer.setLayer(features));
+    // reset del focus
+    status.observe.filter(state => 'reset' in state).subscribe(() => {
+        mGrid.resetStyle();
+        vGrid.resetStyle();
+        fLayer.clearLayers();
+    });
 
-    e.originalEvent.preventDefault();
+    /*
+     * Gestione eventi mappa
+     */
+    vGrid.on('click', e => {
+        if (e.originalEvent.defaultPrevented) {
+            return;
+        }
+        e.originalEvent.preventDefault();
 
-    console.debug('click event at', e.latlng);
-    // recupero focus se attuale
-    let focus = status.getFocus();
-    if (focus) {
-        // console.debug('click inside focus area',focus);
-        let pt = turf.point([e.latlng.lng, e.latlng.lat]);
-        let geoJSON = { type: "FeatureCollection", features: focus.features };
-        console.debug('within?', pt, geoJSON);
-        let result = within(turf.featureCollection([pt]), geoJSON);
-        console.debug('within?', result);
-        if (result.features.length < 1) {
-            status.restore();
+        console.debug('click event at', e.latlng);
+        // recupero focus se attuale
+        let focus = status.getFocus();
+        if (focus) {
+            // console.debug('click inside focus area',focus);
+            let pt = turf.point([e.latlng.lng, e.latlng.lat]);
+            let geoJSON = { type: "FeatureCollection", features: focus.features };
+            console.debug('within?', pt, geoJSON);
+            let result = within(turf.featureCollection([pt]), geoJSON);
+            console.debug('within?', result);
+            if (result.features.length < 1) {
+                status.restore();
+            } else {
+                status.focus(e.layer.properties);
+            }
         } else {
+            // azione focus
             status.focus(e.layer.properties);
         }
-    } else {
-        // azione focus
-        status.focus(e.layer.properties);
-    }
-});
-// prima del cambio di zoom
-map.on('moveend', e => {
-    // update della posizione nello stato
-    status.move(map.getBounds());
-});
-// fine cambio di zoom
-map.on('zoomend', e => {
-    // aggiorno stile marker
-    // todo gestione focus nella scelta di stile
-    mGrid.update();
-});
+    });
+    // prima del cambio di zoom
+    map.on('moveend', e => {
+        // update della posizione nello stato
+        status.move(map.getBounds());
+    });
+    // fine cambio di zoom
+    map.on('zoomend', e => {
+        // aggiorno stile marker
+        // todo gestione focus nella scelta di stile
+        mGrid.update();
+    });
+}();
 
 },{"./js/datasource.js":2,"./js/focus":3,"./js/interactive.js":4,"./js/map":5,"./js/status":6,"./js/utils":7,"./libs/Leaflet.VectorGrid":8,"./libs/leaflet-geojson-gridlayer":9,"@turf/helpers":10,"@turf/within":13,"leaflet":14}],2:[function(require,module,exports){
 /**
