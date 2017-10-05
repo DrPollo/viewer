9bac16fd7cb0ec5eed0aa62f33660f41e5a49bdc
Alessio <antonini@di.unito.it>
2017-10-05 11:27:57 +0200

    fix params on ie

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 // console.log(this);
 
 
-
 // defaults
 var zoom = null;
 var locationZoom = 18;
@@ -13,7 +12,7 @@ var lat = null;
 var lon = null;
 var baseColor = '#c32630';
 var mapOptions = {
-    center: [lat,lon],
+    center: [lat, lon],
     zoom: zoom
 };
 var zoomControlPosition = 'bottomright';
@@ -35,8 +34,8 @@ var mode = false;
 var params = null;
 var focus = null;
 var date = {
-    from:null,
-    to:null
+    from: null,
+    to: null
 };
 var priority = {
     highlight: [],
@@ -49,92 +48,96 @@ var label = document.getElementById('label');
 
 // language
 var defaultLang = 0;
-var languages = ['en','it'];
+var languages = ['en', 'it'];
 var userLang = navigator.language || navigator.userLanguage;
 var lang = languages[defaultLang];
-for(var i = 0; i < languages.length; i++){
+for (var i = 0; i < languages.length; i++) {
     var l = languages[i];
-    if(userLang.search(l) > -1){
+    if (userLang.search(l) > -1) {
         lang = l;
     }
 }
 
 // recover search params
+if (!window.location.getParameter) {
+    window.location.getParameter = function (key) {
+        function parseParams() {
+            var params = {},
+                e,
+                a = /\+/g,  // Regex for replacing addition symbol with a space
+                r = /([^&=]+)=?([^&]*)/g,
+                d = function (s) {
+                    return decodeURIComponent(s.replace(a, " "));
+                },
+                q = window.location.search.substring(1);
 
-// check for IE
-var ua = window.navigator.userAgent;
-var msie = ua.indexOf("MSIE ");
+            while (e = r.exec(q))
+                params[d(e[1])] = d(e[2]);
 
-// If Internet Explorer, return version number
-if (msie > 0) {
-    params = escape(location.search);
-}else{
-    params = (new URL(location)).searchParams;
+            return params;
+        }
+
+        if (!this.queryStringParams)
+            this.queryStringParams = parseParams();
+
+        return this.queryStringParams[key];
+    };
 }
+
 
 var currentParams = {};
 
-if(params){
-// override location from get params
-//     lat = params.get('lat') ? params.get('lat') : lat;
-//     lon = params.get('lon') ? params.get('lon') : lon;
-//     zoom = params.get('zoom') ? params.get('zoom') : zoom;
-//     currentParams.c = lat+':'+lon+':'+zoom;
-    if(params.get('c')){
-        currentParams.c = params.get('c');
-        let cArray = currentParams.c.split(':');
-        lat = cArray[0];
-        lon = cArray[1];
-        zoom = cArray[2];
-    }else{
-        currentParams.c = lat+':'+lon+':'+zoom;
-    }
-    contrast = params.get('contrast') === 'true';
-    currentParams.contrast = contrast;
-    lang = params.get('lang') ? params.get('lang') : lang;
-    currentParams.lang = lang;
-    focus = params.get('focus') ? params.get('focus') : false;
-    currentParams.focus = focus;
 
-    // if domain does not exist trows a console error
-    if(params.get('domain')){
-        // recover domain param (used for security reasons)
-        domain = params.get('domain');
-    }
-    currentParams.domain = domain;
-    // handling date_from param
-    if(params.get('date_from') && Date.parse(params.get('date_from'))){
-        date.from = params.get('date_from');
-        currentParams.date_from = date.to;
-    }
-    // handling date_to param
-    if(params.get('date_to') && Date.parse(params.get('date_to'))){
-        date.to = params.get('date_to');
-        currentParams.date_to = date.to;
-    }
-    // fix time validity
-    checkTime();
-    // handling priority params
-    if(params.get('highlight')){
-        priority.highlight = params.get('highlight').split(",") || [];
-        currentParams.highlight = priority.highlight;
-    }
-    if(params.get('background')){
-        priority.background = params.get('background').split(",") || [];
-        currentParams.background = priority.background;
-    }
-    if(params.get('exclude')){
-        priority.exclude = params.get('exclude').split(",") || [];
-        currentParams.exclude = priority.exclude;
-    }
-    if(params.get('interactive') !== null){
-        currentParams.interactive = params.get('interactive');
-    }
-}else{
-    console.error('cannot retrieve search params from URL location');
+if (window.location.getParameter('c')) {
+    currentParams.c = window.location.getParameter('c');
+    let cArray = currentParams.c.split(':');
+    lat = cArray[0];
+    lon = cArray[1];
+    zoom = cArray[2];
+} else {
+    currentParams.c = lat + ':' + lon + ':' + zoom;
 }
+contrast = window.location.getParameter('contrast') === 'true';
+currentParams.contrast = contrast;
+lang = window.location.getParameter('lang') ? window.location.getParameter('lang') : lang;
+currentParams.lang = lang;
+focus = window.location.getParameter('focus') ? window.location.getParameter('focus') : false;
+currentParams.focus = focus;
 
-
+// if domain does not exist trows a console error
+if (window.location.getParameter('domain')) {
+    // recover domain param (used for security reasons)
+    domain = window.location.getParameter('domain');
+}
+currentParams.domain = domain;
+// handling date_from param
+if (window.location.getParameter('date_from') && Date.parse(window.location.getParameter('date_from'))) {
+    date.from = window.location.getParameter('date_from');
+    currentParams.date_from = date.to;
+}
+// handling date_to param
+if (window.location.getParameter('date_to') && Date.parse(window.location.getParameter('date_to'))) {
+    date.to = window.location.getParameter('date_to');
+    currentParams.date_to = date.to;
+}
+// fix time validity
+checkTime();
+// handling priority params
+if (window.location.getParameter('highlight')) {
+    priority.highlight = window.location.getParameter('highlight').split(",") || [];
+    currentParams.highlight = priority.highlight;
+}
+if (window.location.getParameter('background')) {
+    priority.background = window.location.getParameter('background').split(",") || [];
+    currentParams.background = priority.background;
+}
+if (window.location.getParameter('exclude')) {
+    priority.exclude = window.location.getParameter('exclude').split(",") || [];
+    currentParams.exclude = priority.exclude;
+}
+if (window.location.getParameter('interactive') !== null) {
+    currentParams.interactive = window.location.getParameter('interactive');
+}
 
 
 /*
@@ -158,7 +161,6 @@ var focusToEvent = "areaViewer.focusTo";
 var toExploreEvent = "areaViewer.toExplore";
 
 
-
 /*
  * Message broker
  * Output messages: receives messages from AreaViewer and emit them to top
@@ -167,7 +169,7 @@ var toExploreEvent = "areaViewer.toExplore";
 
 
 // input messages
-window.addEventListener( "message",
+window.addEventListener("message",
     function (e) {
         if (e.defaultPrevented)
             return
@@ -176,18 +178,18 @@ window.addEventListener( "message",
         // if (e.origin !== iframeDomain) {
         //     return;
         // }
-        if(e.data.type == 'setContrast'){
-            console.log('got setContrast',e);
+        if (e.data.type == 'setContrast') {
+            console.log('got setContrast', e);
         }
     });
 
 /*
  * Init AreaViewer status
  */
-function initAreaViewer(){
+function initAreaViewer() {
     console.debug("init AreaViewer");
     initListners();
-    initStatus ();
+    initStatus();
 
     // test listners
     // setTimeout(function(){broadcastEvent(focusToEvent,{id:"dc1346b2-8d8f-11e7-94d0-6558e55bfac7"});},2000);
@@ -196,38 +198,38 @@ function initAreaViewer(){
 }
 
 // init status
-function initStatus (){
+function initStatus() {
     // console.debug('init status');
     // set default lang
-    if(lang){
-        broadcastEvent(setLanguageEvent,{lang: lang});
+    if (lang) {
+        broadcastEvent(setLanguageEvent, {lang: lang});
     }
     // set default contrast
-    if(contrast){
-        broadcastEvent(setContrastEvent,{contrast: contrast});
+    if (contrast) {
+        broadcastEvent(setContrastEvent, {contrast: contrast});
     }
     // set default viewport
-    if(lat && lon && zoom){
-        broadcastEvent(setViewEvent, {lat: lat, lng:lon, zoom:zoom});
+    if (lat && lon && zoom) {
+        broadcastEvent(setViewEvent, {lat: lat, lng: lon, zoom: zoom});
     }
     // set interativity
-    if(currentParams.interactive) {
+    if (currentParams.interactive) {
         broadcastEvent(setInteractivityEvent, {interactive: currentParams.interactive});
     }
     // set focus
-    if(focus && focus !== 'null') {
-        setTimeout(function(){
-            broadcastEvent(focusToEvent,{id:focus});
-        },1500);
+    if (focus && focus !== 'null') {
+        setTimeout(function () {
+            broadcastEvent(focusToEvent, {id: focus});
+        }, 1500);
     }
     // set date
-    if(date.form !== 'null' && date.to !== 'null') {
-        broadcastEvent(setDateEvent,{date_from:date.from,date_to:date.to});
+    if (date.form !== 'null' && date.to !== 'null') {
+        broadcastEvent(setDateEvent, {date_from: date.from, date_to: date.to});
     }
     // set priority: {highlight:[], background:[], exclude:[]}
-    console.debug('check priority',priority);
-    if(priority.highlight.length > 0 || priority.background.length > 0 || priority.exclude.length > 0){
-        broadcastEvent(setPriorityEvent,{priority:priority});
+    console.debug('check priority', priority);
+    if (priority.highlight.length > 0 || priority.background.length > 0 || priority.exclude.length > 0) {
+        broadcastEvent(setPriorityEvent, {priority: priority});
     }
 }
 
@@ -235,30 +237,33 @@ function initStatus (){
 function initListners() {
     // console.debug("init output listners")
     // enter in focus mode
-    document.addEventListener(focusOnEvent,function (e) {
-        console.debug(focusOnEvent,e.detail);
+    document.addEventListener(focusOnEvent, function (e) {
+        console.debug(focusOnEvent, e.detail);
         //change search param > add focus=id
-        if(!e.detail.id){return;}
-        updateQueryParams('focus',e.detail.id);
-        emitEvent(focusOnEvent,e.detail);
-    },true);
+        if (!e.detail.id) {
+            return;
+        }
+        updateQueryParams('focus', e.detail.id);
+        emitEvent(focusOnEvent, e.detail);
+    }, true);
     // back to explore mode
-    document.addEventListener(exploreEvent,function (e) {
-        console.debug(exploreEvent,e.detail);
+    document.addEventListener(exploreEvent, function (e) {
+        console.debug(exploreEvent, e.detail);
         //change search param > remove focus=id
-        updateQueryParams('focus',null);
-        emitEvent(exploreEvent,e.detail);
-    },true);
+        updateQueryParams('focus', null);
+        emitEvent(exploreEvent, e.detail);
+    }, true);
     // update position
-    document.addEventListener(changePositionEvent,function (e) {
-        console.debug(changePositionEvent,e.detail);
+    document.addEventListener(changePositionEvent, function (e) {
+        console.debug(changePositionEvent, e.detail);
         // update c param
-        if(!e.detail.c){return;}
-        updateQueryParams('c',e.detail.c);
-        emitEvent(changePositionEvent,e.detail);
+        if (!e.detail.c) {
+            return;
+        }
+        updateQueryParams('c', e.detail.c);
+        emitEvent(changePositionEvent, e.detail);
     });
 }
-
 
 
 /*
@@ -269,33 +274,39 @@ function initListners() {
 
 function broadcastEvent(eventName, params) {
     var detail = params || {};
-    var event = new CustomEvent(eventName, {detail: detail });
+    var event = new CustomEvent(eventName, {detail: detail});
     // console.debug('broadcasting',eventName,event.detail);
     document.dispatchEvent(event);
 }
 function emitEvent(eventName, params) {
-    if(!domain){
-        console.error("domain not defined: cannot emit message",eventName);
+    if (!domain) {
+        console.error("domain not defined: cannot emit message", eventName);
         return;
     }
     // console.debug('emitting',params, 'to',domain);
-    top.postMessage(Object.assign(params || {},{"event":eventName}), domain);
+    top.postMessage(Object.assign(params || {}, {"event": eventName}), domain);
 }
 
 
 // update location.search params
 function updateQueryParams(key, value) {
     // console.debug('check updateQueryParams',!currentParams);
-    if (!currentParams) {return;}
+    if (!currentParams) {
+        return;
+    }
     // console.debug('check updateQueryParams',location.search === null);
-    if(location.search === null){ return;}
+    if (location.search === null) {
+        return;
+    }
     // console.debug('check updateQueryParams',!window.history);
-    if(!window.history){ return;}
+    if (!window.history) {
+        return;
+    }
     // console.debug('updating location.search params', key,value);
-    if(value !== null && value !== "null"){
+    if (value !== null && value !== "null") {
         // aggiungo chiave e valore
         currentParams[key] = value;
-    }else if(currentParams[key]){
+    } else if (currentParams[key]) {
         // rimuovo chiave
         delete currentParams[key];
     }
@@ -304,46 +315,49 @@ function updateQueryParams(key, value) {
     // history.replaceState(stateParams,'AreaViewer');
     var q = Object.keys(currentParams).reduce(function (res, key) {
         // null value
-        if(currentParams[key] === null || currentParams[key] === "null") { return res;}
+        if (currentParams[key] === null || currentParams[key] === "null") {
+            return res;
+        }
 
         var val = '';
-        if(res !== '?'){ val = val.concat('&'); }
-        val = val.concat(key,'=',currentParams[key]);
+        if (res !== '?') {
+            val = val.concat('&');
+        }
+        val = val.concat(key, '=', currentParams[key]);
         return res.concat(val);
-    },'?');
+    }, '?');
     // console.debug('check new params',q);
     // console.debug('new location.search value? ',(location.search !== q),' ',location.search,' !== ',q);
-    if(location.search !== q){
+    if (location.search !== q) {
         // location.search = q;
         window.history.replaceState(null, null, q);
     }
 }
 
 
-
-function checkTime(){
-    console.log('checkTime',date);
+function checkTime() {
+    console.log('checkTime', date);
     // case valid_from and valid_to defined
     // check intersection date.from after date.to
-    if(date.from && date.to && Date.parse(date.from) > Date.parse(date.to) ){
+    if (date.from && date.to && Date.parse(date.from) > Date.parse(date.to)) {
         // forcing date.from = date.to
         date.from = date.to;
         currentParams.date_from = date.from;
-        updateQueryParams('date_from',date.from);
+        updateQueryParams('date_from', date.from);
     }
     // check valid_from not defined valid_to defined
-    if(!date.from && date.to){
+    if (!date.from && date.to) {
         console.log('adding date_from')
         // forcing date.from = date.to
         date.from = date.to;
         currentParams.date_from = date.from;
-        updateQueryParams('date_from',date.from);
+        updateQueryParams('date_from', date.from);
     }
     // check valid_to not defined and valid_from defined
-    if(!date.to && date.from){
+    if (!date.to && date.from) {
         // forcing date.to = date.from
         date.to = date.from;
         currentParams.date_to = date.to;
-        updateQueryParams('date_to',date.to);
+        updateQueryParams('date_to', date.to);
     }
 }
